66d80f34662ab7eade74423f60a438a0b471792e
Philip Milliorn <scottmilliorn@outlook.com>
2020-12-23 19:10:58 -0800

    budget.json

--- lighthouserc.js
+++ lighthouserc.js
@@ -13,9 +13,10 @@ module.exports = {
             target: 'temporary-public-storage',
         },
         assert: {
+            //budgetFile: './budget.json',
             preset: 'lighthouse:no-pwa',
             assertions: {
-                'categories:performance': ['error', { minScore: 0.1 }],
+                'categories:performance': ['error', { minScore: 0. }],
                 'categories:accessibility': ['warn', { minScore: 0.1 }],
             },
         },
@@ -24,6 +25,8 @@ module.exports = {
 
 
 /*
+https://css-tricks.com/continuous-performance-analysis-with-lighthouse-ci-and-github-actions/
+
 lighthouse:all: Asserts that every audit received a perfect score
 lighthouse:recommended: Asserts that every audit outside performance received a perfect score, and warns when metric values drop below a score of 90
 lighthouse:no-pwa: The same as lighthouse:recommended but without any of the PWA audits
