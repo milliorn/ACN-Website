a061f17a0a5ac65a641c5c7a7f043ba75c14cba8
Philip Milliorn <scottmilliorn@outlook.com>
2020-12-23 18:56:27 -0800

    more options

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,6 +5,9 @@ module.exports = {
             staticDistDir: './public',
             //startServerCommand: 'npm run server',
             //url: ['http://localhost:4000/'],
+            //startServerReadyPattern: 'Server is running on PORT 4000',
+            //startServerReadyTimeout: 20000 // milliseconds
+            //numberOfRuns: 5,
         },
         upload: {
             target: 'temporary-public-storage',
