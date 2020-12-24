3955cdf1de5f873e89256916d710d0ec31c8940c
Philip Milliorn <scottmilliorn@outlook.com>
2020-12-23 18:55:50 -0800

    added url

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,6 +3,8 @@ module.exports = {
     ci: {
         collect: {
             staticDistDir: './public',
+            //startServerCommand: 'npm run server',
+            //url: ['http://localhost:4000/'],
         },
         upload: {
             target: 'temporary-public-storage',
